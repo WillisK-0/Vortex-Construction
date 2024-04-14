e5b3d1a7dbb4bbed4ec076f7fa502a607672597b
Keith Willis <keith.willis@indysoft.com>
2024-04-14 14:23:29 -0500

    fix video

--- components/Home3/Home3Hero.js
+++ components/Home3/Home3Hero.js
@@ -8,69 +8,57 @@ export default function Home3Hero() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className="fn_cs_hero_header">
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "800px",
+          overflow: "hidden",
+        }}
+      >
+        <iframe
+          style={{
+            position: "absolute",
+            display: "block",
+            border: "none",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+          }}
+          width="100%"
+          height="100%"
+          src="https://player.vimeo.com/video/933918789?autoplay=1&loop=1&autopause=0&muted=1&title=0&controls=0"
+          frameBorder="0"
+          allow="autoplay; fullscreen"
+        ></iframe>
+
         <div
+          className="container"
           style={{
             position: "relative",
-            width: "100%",
-            height: "800px",
-            overflow: "hidden",
+            zIndex: 1000,
+            height: "100%",
+            padding: "15px",
           }}
         >
-          <iframe
-            style={{
-              position: "absolute",
-              top: "0",
-              left: "0",
-              width: "100%",
-              height: "100%",
-            }}
-            width="100%"
-            height="100%"
-            src="https://player.vimeo.com/video/933918789?autoplay=1&loop=1&autopause=0&muted=1&title=0&controls=0"
-            frameBorder="0"
-            allow="autoplay; fullscreen"
-          ></iframe>
-
-          <div
-            className="container"
-            style={{
-              position: "relative",
-              zIndex: 1000,
-              height: "100%",
-              padding: "15px",
-            }}
-          >
-            <div className="title_holder">
-              <div
-                style={{
-                  //   backgroundColor: "#090e2c",
-                  textAlign: "center",
-                  fontSize: "20px",
-                }}
-              >
-                <img
-                  className="desktop_logo"
-                  src="/img/word-logo.png"
-                  alt="Industry HTML Template"
-                  style={{ width: "750px" }}
-                />
-
-                {/* <h1>Vortex Construction</h1> */}
-              </div>
+          <div className="title_holder_logo">
+            <div
+              style={{
+                //   backgroundColor: "#090e2c",
+                textAlign: "center",
+                fontSize: "20px",
+              }}
+            >
+              <img
+                className="desktop_logo"
+                src="/img/word-logo.png"
+                alt="Industry HTML Template"
+                style={{ width: "750px" }}
+              />
             </div>
           </div>
         </div>
-
-        {/* <div
-            className="o_img"
-            style={{ background: `url(${headerBG})` }}
-          ></div> */}
-        {/* <img
-            // className="desktop_logo"
-            src="/img/video/landingVid.mp4"
-            alt="Industry HTML Template"
-          /> */}
       </div>
       {/* <ModalVideo
         channel="youtube"
